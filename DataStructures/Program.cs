fb2901d4e0b12f212a6e283704d52eb962e8e8c0
Corey Gossett <coreymgossett@gmail.com>
2023-02-03 22:58:52 -0500

    Declare Pokemon

--- DataStructures/Program.cs
+++ DataStructures/Program.cs
@@ -1,10 +1,15 @@
-﻿namespace DataStructures
+﻿using DataStructures.ADT;
+
+namespace DataStructures
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Pokemon squirtle = new Pokemon();
+
+            squirtle.Name = "Squirtle";
+
         }
     }
 }
